9d4cc2623c1ff9f22f0ab7fb3c224e10be1c594a
Bashar403 <133667040+Bashar403@users.noreply.github.com>
2023-10-09 18:20:22 -0400

    Add project files.

---
+++ zipperzing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace zipperzing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
